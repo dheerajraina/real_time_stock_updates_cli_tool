b45758cee4796d9d7d63ea1fbe3ea514e0720293
Dheeraj Raina <dheerajraina55@gmail.com>
2024-01-22 13:44:20 +0530

    Interface to call python code from golang

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 package main
 
-import "fmt"
-func main(){
-	fmt.Println("Hello World!")
+import (
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+
+	arg1:="INFY"
+
+	cmd := exec.Command("python3", "web_scraper.py",arg1)
+
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println("Error executing Python script", err)
+		return
+	}
+	result := string(output)
+	fmt.Println("Result from Python script", result)
 }
